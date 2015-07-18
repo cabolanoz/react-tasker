736a7e496d93fe6363a8b9d1396ee011c5412dd2
ceasar <cesar.bolanos.andino@gmail.com>
2015-07-18 01:19:17 -0500

    Creating new task react component

--- app/assets/javascripts/components/tasks/new_task_form.js
+++ app/assets/javascripts/components/tasks/new_task_form.js
@@ -9,6 +9,26 @@ class NewTaskForm extends BaseComponent {
       title: '',
       description: ''
     }
+
+    this._bind('onTitleChange', 'onDescriptionChange');
+  }
+
+  onTitleChange(event) {
+    this.setState({ title: event.target.value });
+  }
+
+  onDescriptionChange(event) {
+    this.setState({ description: event.target.value });
+  }
+
+  render() {
+    return(
+      <form className="form-horizontal">
+        <fieldset>
+          <legend>New Task</legend>
+        </fieldset>
+      </form>
+    );
   }
 
 }
