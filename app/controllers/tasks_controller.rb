a83c9abefc8e9f1942385f5ea498be08c2be3f1a
ceasar <cesar.bolanos.andino@gmail.com>
2015-07-18 03:24:34 -0500

    Adding create function on controller for saving the task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,22 @@ class TasksController < ApplicationController
       format.html
     end
   end
+
+  def create
+    task = Task.new(task_params)
+    respond_to do |format|
+      if task.save
+        format.html { redirect_to tasks_path }
+        format.json { render json: task, status: :created }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:title, :description);
+    end
 end
