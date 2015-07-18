ab72e92726cf72e7cd918f3ec786c62ea8a13bb3
ceasar <cesar.bolanos.andino@gmail.com>
2015-07-18 01:34:47 -0500

    Adding extra inputs to react form component

--- app/assets/javascripts/components/tasks/new_task_form.es6.jsx
+++ app/assets/javascripts/components/tasks/new_task_form.es6.jsx
@@ -26,6 +26,18 @@ class NewTaskForm extends BaseComponent {
       <form className="form-horizontal">
         <fieldset>
           <legend>New Task</legend>
+          <div className="form-group">
+            <label className="col-lg-2 control-label" htmlFor="title">Title</label>
+            <div className="col-lg-10">
+              <input className="form-control" id="title" onChange={ this.onTitleChange } placeholder="Title" type="text" value={ this.state.title } />
+            </div>
+          </div>
+          <div className="form-group">
+            <label className="col-lg-2 control-label" htmlFor="description">Description</label>
+            <div className="col-lg-10">
+              <input className="form-control" id="description" onChange={ this.onDescriptionChange } placeholder="Description" type="text" value={ this.state.description } />
+            </div>
+          </div>
         </fieldset>
       </form>
     );
